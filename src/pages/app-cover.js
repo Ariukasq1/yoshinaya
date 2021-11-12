de61e9596bb9e39197b6c91ee933595c1689c011
Gereltuya Munkhzaya <gegiimz96@gmail.com>
2021-11-12 14:13:34 +0800

    delete unuseful style files

--- src/pages/app-cover.js
+++ src/pages/app-cover.js
@@ -2,13 +2,28 @@ import React from "react";
 import { Grid, Box } from "@mui/material";
 
 const AppCover = () => {
-	const applicationCover = "/images/applicationCover.svg";
+	const appPhone = "/images/phones.png";
+	const playStore = "/images/playStore.svg";
+	const appStore = "/images/appStore.svg";
 
 	return (
 		<div className="component center" key="home-app-cover">
-			<Grid item xs={12} md={12}>
-				<img src={applicationCover} alt="application cover" />
-			</Grid>
+			<div className="background-style">
+				<Grid item xs={12} md={5}>
+					<h3>
+						Та манай аппликейшиныг татсанаар олон онцгой боломжууд хүлээж байна.
+					</h3>
+					<div className="store-images">
+						<img src={appStore} alt="app store" />
+						<img src={playStore} alt="play store" />
+					</div>
+				</Grid>
+				<Grid item sx={12} md={7}>
+					{/* <div className="app-cover-img"> */}
+						<img src={appPhone} alt="application phone" />
+					{/* </div> */}
+				</Grid>
+			</div>
 		</div>
 	);
 };

185d1e469a5c00d55e50c7ac90fb77b6975385b6
Gereltuya Munkhzaya <gegiimz96@gmail.com>
2021-11-14 16:23:18 +0800

    fix footer icons effect and modify some css files

--- src/components/layouts/footer.js
+++ src/components/layouts/footer.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { Container, Typography } from "@mui/material";
+import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
 
 const Footer = () => {
 	const yoshinoyaLogo = "/images/YoshinoyaLogo.svg";
@@ -18,6 +21,34 @@ const Footer = () => {
 						<div className="left-side">
 							<div className="icons">
 								<Typography color="#616E7C">Follow us:</Typography>
+								<a
+									href="https://www.facebook.com/YoshinoyaMongolia"
+									target="_blank"
+									rel="noopener noreferrer"
+									className="facebook-link"
+								>
+									<FacebookRoundedIcon />
+								</a>
+								<a
+									href="https://twitter.com/"
+									target="_blank"
+									rel="noopener noreferrer"
+									className="twitter-link"
+								>
+									<TwitterIcon />
+								</a>
+								<a
+									href="https://www.instagram.com/Yoshinoyamongolia/?fbclid=IwAR3CEMk2gsst4Nwy3NuWDdUAKLIuTaljtOyHJlZxGQylPbesdFdGFwR3aqQ"
+									target="_blank"
+									rel="noopener noreferrer"
+									className="instagram-link"
+								>
+									<div className="bg">
+										<div className="inner">
+											<InstagramIcon />
+										</div>
+									</div>
+								</a>
 							</div>
 							<Typography color="black">
 								Зохиогчийн эрхээр хамгаалагдсан 2021 он

41bc278a8c0023fad4e04a374c1874319bb17fa9
Daariimaa <daarii@Daariimaas-MacBook-Pro.local>
2021-11-17 17:58:46 +0800

    added favicon

--- src/components/layouts/header.js
+++ src/components/layouts/header.js
@@ -11,11 +11,14 @@ function Header({ title, image }) {
   return (
     <>
       <Head>
-        <title>{title || "Gerege WP Template"}</title>
+        <title>{title || "YOSHINOYA"}</title>
 
         <meta charSet="UTF-8" />
         <meta name="keywords" content="wp, gerege" />
-        <meta name="description" content="Here is website description" />
+        <meta
+          name="description"
+          content="Yoshinoya нь Япон улсын хамгийн алдартай түргэн хоолны сүлжээ ресторануудын нэг бөгөөд дэлхийн хамгийн анхны Gyudon буюу Үхрийн махтай боулаар “Амттай, Энгийн, Түргэн” гэсэн уриан доор 122 жилийн турш одоог хүртэл үйл ажиллагаа явуулж дэлхийн хамгийн анхны ресторан юм. "
+        />
         <meta name="author" content="Here is website author url" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
@@ -26,32 +29,33 @@ function Header({ title, image }) {
         <meta httpEquiv="Pragma" content="no-cache" />
         <meta httpEquiv="Expires" content="0" />
 
-        <meta property="og:title" content={title || "Gerege WP Template"} />
-        <meta property="og:description" content="Here is website description" />
+        <meta property="og:title" content={title || "YOSHINOYA"} />
+        <meta
+          property="og:description"
+          content="Yoshinoya нь Япон улсын хамгийн алдартай түргэн хоолны сүлжээ ресторануудын нэг бөгөөд дэлхийн хамгийн анхны Gyudon буюу Үхрийн махтай боулаар “Амттай, Энгийн, Түргэн” гэсэн уриан доор 122 жилийн турш одоог хүртэл үйл ажиллагаа явуулж дэлхийн хамгийн анхны ресторан юм. "
+        />
         <meta property="og:url" content={defaultUrl} />
         <meta property="og:type" content="website" />
-        <meta
-          property="og:image"
-          content={image || "Here is website demo image"}
-        />
-        <meta property="og:site_name" content="Gerege WP Template" />
+        <meta property="og:image" content={image || "/images/yoshinoya.jpg"} />
+        <meta property="og:site_name" content="YOSHINOYA" />
 
         <meta name="twitter:card" content="summary" />
-        <meta name="twitter:title" content={title || "Gerege WP Template"} />
+        <meta name="twitter:title" content={title || "YOSHINOYA"} />
         <meta name="twitter:url" content={defaultUrl} />
         <meta
           name="twitter:description"
-          content="Here is website description"
+          content="Yoshinoya нь Япон улсын хамгийн алдартай түргэн хоолны сүлжээ ресторануудын нэг бөгөөд дэлхийн хамгийн анхны Gyudon буюу Үхрийн махтай боулаар “Амттай, Энгийн, Түргэн” гэсэн уриан доор 122 жилийн турш одоог хүртэл үйл ажиллагаа явуулж дэлхийн хамгийн анхны ресторан юм. "
         />
-        <meta
-          name="twitter:image"
-          content={image || "Here is website demo image"}
-        />
+        <meta name="twitter:image" content={image || "/images/yoshinoya.jpg"} />
         <meta name="twitter:site" content="@your-site-twitter-name" />
         <meta name="twitter:creator" content="@your-site-twitter-name" />
-        <meta name="twitter:image:alt" content="Gerege WP Template" />
+        <meta name="twitter:image:alt" content="YOSHINOYA" />
 
-        <link rel="shortcut icon" id="favicon" href="/favicon.ico"></link>
+        <link
+          rel="shortcut icon"
+          id="favicon"
+          href="/images/yoshi-icon.svg"
+        ></link>
         <link
           rel="stylesheet"
           type="text/css"
